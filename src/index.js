a0abb07b344a1219652a2d08a6f1f889d07a1f06
Mona Gupta <guptamona915@gmail.com>
2025-04-03 19:09:23 +0530

    connect to mongo compass

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config()
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+  path: './env'
+})
+
+
+
+
+ connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
